7aca1d359740d831c9f80f1e6a34c9722ea419ae
jauhc <paperslove420@gmail.com>
2020-01-17 10:32:49 +0200

    finally added kill/death events

--- main.go
+++ main.go
@@ -4,9 +4,11 @@ import (
 	"log"
 	"os"
 	"strings"
+	"sync"
+	"syscall"
 	"time"
 
-	"github.com/dank/go-csgsi"
+	"github.com/jauhc/go-csgsi"
 
 	"github.com/ziutek/telnet"
 )
@@ -23,6 +25,7 @@ import (
 		!% logics
 		% wrapper for timers
 		? location parser with m_szLastPlaceName and "getpos"
+		! error checks
 	just rewrite shit tbh
 
 	options
@@ -82,62 +85,107 @@ func listenerLoop(t *telnet.Conn) {
 }
 
 // check if given gun IS a gun
-func isGun(gsi *csgsi.State) bool {
+func isGun(gun *csgsi.Weapon) bool {
 	// taser returns as ""
-	w := strings.ToLower(getActiveGunType(gsi))
-	switch w {
-	case "pistol":
-		return true
-	case "rifle":
-		return true
-	case "sniperrifle":
-		return true
-	case "machine gun":
-		return true
-	case "submachine gun":
-		return true
-	case "shotgun":
-		return true
-	default:
-		return false
+	if gun != nil {
+
+		w := strings.ToLower(gun.Type)
+		switch w {
+		case "pistol":
+			return true
+		case "rifle":
+			return true
+		case "sniperrifle":
+			return true
+		case "submachine gun":
+			return true
+		case "machine gun":
+			return true
+		case "shotgun":
+			return true
+		default:
+			return false
+		}
+	}
+	return false
+}
+
+// forked a repo just to export structs lole
+// gets given player's active weapon
+func getActiveGun(gsi *csgsi.State) *csgsi.Weapon {
+	for w := range gsi.Player.Weapons {
+		if gsi.Player.Weapons[w].State == "active" {
+			return gsi.Player.Weapons[w]
+		}
+	}
+	return nil
+}
+
+func ammoWarning(state *csgsi.State) {
+	w := getActiveGun(state)
+	if isGun(w) {
+		if float32(w.Ammo_clip)/float32(w.Ammo_clip_max) < 0.3 &&
+			w.Ammo_clip_max > 1 {
+			// make check if we want this on or not
+			beep.Call(80, 168)
+		}
 	}
 }
 
-// gets given player's active weapon
-func getActiveGunType(gsi *csgsi.State) string {
-	for w := range gsi.Player.Weapons {
-		if gsi.Player.Weapons[w].State == "active" {
-			return gsi.Player.Weapons[w].Type
+// fuck, redo; previous ONLY updates when worth updating
+func killCheck(state *csgsi.State) {
+	if state.Previously != nil {
+		if state.Previously.Player != nil {
+			if state.Previously.Player.Match_stats != nil {
+				// i feel like im missing something very important
+				if state.Previously.Player.Match_stats.Kills < state.Player.Match_stats.Kills {
+					log.Println("KILL")
+					log.Printf("curkills: %d", state.Player.Match_stats.Kills)
+					log.Printf("prevkills: %d", state.Previously.Player.Match_stats.Kills)
+					return
+				}
+			}
 		}
 	}
-	return ""
 }
 
-// would be better to just return weapon struct in one method?
-
-// gets given player's active weapon
-func getActiveGunName(gsi *csgsi.State) string {
-	for w := range gsi.Player.Weapons {
-		if gsi.Player.Weapons[w].State == "active" {
-			return gsi.Player.Weapons[w].Name
+// check for added instead
+func deathCheck(state *csgsi.State) {
+	if state.Previously != nil {
+		if state.Previously.Player != nil {
+			if state.Previously.Player.Match_stats != nil {
+				// i feel like im missing something very important
+				if state.Previously.Player.Match_stats.Deaths < state.Player.Match_stats.Deaths &&
+					state.Previously.Player.Match_stats.Deaths > 0 {
+					log.Println("DETH")
+					log.Printf("curdeth: %d", state.Player.Match_stats.Deaths)
+					log.Printf("prevdeth: %d", state.Previously.Player.Match_stats.Deaths)
+					return
+				}
+			}
 		}
 	}
-	return ""
 }
 
 // main logic here with game events
 func stateParser(gsi *csgsi.Game) {
 	go func() {
 		for state := range gsi.Channel {
-			log.Println(state.Player.Name)
 			if state.Round.Phase == "live" || 1 == 1 {
-				log.Println("live")
+				// log.Println("live")
 				// logics here
-				log.Println(getActiveGunType(&state))
+				ammoWarning(&state) // warms when ammo is low :)
+				killCheck(&state)
+				deathCheck(&state)
 			}
 		}
 	}()
 }
+
+var (
+	beep      = syscall.MustLoadDLL("Kernel32.dll").MustFindProc("Beep")
+	stateWait sync.Once
+)
 
 func main() {
 	log.Println("---START---")
